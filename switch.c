84565d14258b438f2548f6e5e2ee2535669bf4da
Nithya392 <nithyashree.b@s.amity.edu>
2025-03-08 13:29:32 +0530

    added case 2,3,4

--- switch.c
+++ switch.c
@@ -7,3 +7,13 @@ switch (day) {
 	case 1:
 		printf("Sunday\n");
 		break;
+	case 2:
+		printf("Monday\n");
+		break;
+	case 3:
+		printf("Tuesday\n");
+		break;
+	case 4:
+		printf("Wednesday\n");
+		break;
+
